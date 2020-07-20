49593352503bcb59db9307d89e84899571109cf9
Ivan Achille <ivanachillee@gmail.com>
2020-07-20 14:28:05 -0300

    Updates in library and added test file

--- pcalib.py
+++ pcalib.py
@@ -1,42 +1,48 @@
 
 #PCAlib by Ivan Achille.
-#external packages needed: sklearn
+#external packages needed: sklearn, matplotlib, pandas
+
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-
-
+import matplotlib.pyplot as plt
+import pandas as pd
 
 class dpca():
     def __init__(self, dataset, features, label, components, comp_col_names):
         """
         ENTRY VARIABLES
-        
+
         dataset: pandas df var
         features: array of features of df.     i.e: ["a","b","c"]
         label: label column of df       i.e: ["d"]
         components: n of final principal components
         comp_col_names: array of new columns name of the final principal components     i.e: ["p1","p2"]
 
-        function returns new dataset with final principal components and label column.
+        Function returns new dataset with final principal components and label column.
         """
-        super(pca, self).__init__()
         self.dataset = dataset
         self.features = features
         self.label = label
+        self.components = components
+        self.comp_col_names = comp_col_names
 
         #Gather values from entered features
-        x = dataset.loc[:, features].values
+        self.x = self.dataset.loc[:, self.features].values
 
         #Gather values from entered label (Y)
-        y = dataset.loc[:,[label]].values
+        self.y = self.dataset.loc[:,self.label].values
 
+    def apply(self):
+        
         #Standarize the feature data
-        x = StandardScaler().fit_transform(x)
+        ft = StandardScaler().fit_transform(self.x)
 
-        pca = PCA(n_components=components)
+        pca = PCA(n_components=self.components)
 
-        pComponents = pca.fit_transform(x)
+        pComponents = pca.fit_transform(ft)
         pDf = pd.DataFrame(data = pComponents
-             , columns = [comp_col_names])
+             , columns = [self.comp_col_names])
 
-        fDf = pd.concat([pDf, dataset[[label]]], axis = 1)
+        pca_dataset = pd.concat([pDf, self.dataset[self.label]], axis = 1)
+
+        return pca_dataset #returns pca transformed dataset
---
+++ test.py
@@ -0,0 +1,13 @@
+from pcalib import dpca
+import pandas as pd
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+df = pd.read_csv(url, names=['sepal length','sepal width','petal length','petal width','target'])
+
+features = ['sepal length', 'sepal width', 'petal length', 'petal width']
+label = ['target']
+
+newdf = dpca(df, features, label, 3, ["pc1","pc2"])
+newdf = newdf.apply()
+
+print(newdf.head(5))
